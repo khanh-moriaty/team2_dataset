118d1b02d30fb796f0c0e927d15348cf684ea8b3
khanh-moriaty <63464026+khanh-moriaty@users.noreply.github.com>
2020-08-08 20:58:27 +0700

    Update convert14to5.py
    Added threshold for eliminating very small objects

--- convert14to5.py
+++ convert14to5.py
@@ -2,6 +2,9 @@ import os
 import argparse
 import xml.etree.ElementTree as ET
 
+# If a bbox's edge is not wider than THRESHOLD, the bbox will be eliminated
+THRESHOLD = 5 
+
 parser = argparse.ArgumentParser(description='This script support converting voc format with 14 classes to 5 classes')
 parser.add_argument('--ann_dir', type=str, default=None,
                     help='path to annotation files directory.')
@@ -31,6 +34,17 @@ for fi in dir:
     ann_obj = ann_root.findall('object')
     for obj in ann_obj:
         name = obj.findall('name')[0]
+        bbox = obj.findall('bndbox')[0]
+        xmin = int(bbox.findall('xmin')[0])
+        xmax = int(bbox.findall('xmax')[0])
+        ymin = int(bbox.findall('ymin')[0])
+        ymax = int(bbox.findall('ymax')[0])
+        if xmax-xmin < THRESHOLD:
+            ann_root.remove(obj)
+            continue
+        if ymax-ymin < THRESHOLD:
+            ann_root.remove(obj)
+            continue
         name.text = str(new_class[name.text])
         if name.text == "-1": ann_root.remove(obj)
     ann_tree.write(os.path.join(args.out_dir, fi))
